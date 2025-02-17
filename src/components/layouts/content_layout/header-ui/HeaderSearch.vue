4af8fbcb2b0a27992a582e060e727de09ea3da21
Amir <amirkek80@gmail.com>
2025-02-17 17:36:53 +0500

    Fix: fixed bug with header search appearance

--- src/components/layouts/content_layout/header-ui/HeaderSearch.vue
+++ src/components/layouts/content_layout/header-ui/HeaderSearch.vue
@@ -106,3 +106,52 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+.search-form {
+	position: relative;
+	display: block;
+	width: 300px;
+	height: 42px;
+	&__input {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 1;
+		border: 1px solid #cdcdcd;
+		border-radius: 0.5rem;
+		background-color: transparent;
+		padding: 0.75rem 2.75rem 0.75rem 1.25rem;
+		font-size: 1rem;
+		color: $black;
+		outline: none;
+		appearance: none;
+		transition: all 0.15s ease-in-out;
+		&::placeholder {
+			color: rgba($color: $black, $alpha: 0.5);
+		}
+		&:focus {
+			border-color: $primary;
+			& + .search__icon > i {
+				color: $black;
+			}
+		}
+	}
+	&__icon {
+		position: absolute;
+		top: 50%;
+		right: 1rem;
+		transform: translateY(-50%);
+		z-index: 2;
+		& > i {
+			color: #cdcdcd;
+			transition: all 0.15s ease-in-out;
+		}
+	}
+	&__clear {
+		cursor: pointer;
+	}
+}
+</style>
