e9ba9a99b847e925bd1ed76b9bf20b7dc1198495
Farrux <farruh020202@gmail.com>
2025-02-21 11:07:23 +0500

    Fix: fixed avatar delete api

--- src/api/avatars.js
+++ src/api/avatars.js
@@ -1,45 +1,48 @@
+import { useToast } from "vue-toastification";
 import api from "./axios";
 
+const toast = useToast();
+
 export default {
-    async getAvatars () {
-        try {
-            const response = await api.get('/avatars/');
-            return response.data;
-        }
-        catch (error) {
-            console.log(error);
-        }
-    },
+	async getAvatars() {
+		try {
+			const response = await api.get("/avatars/");
+			return response.data;
+		} catch (error) {
+			console.log(error);
+		}
+	},
 
-    async deleteAvatar (id) {
-        try {
-            const deleted = await api.delete(`/avatars/${id}`);
-            return deleted;
-        }
-        catch (error) {
-            console.log(error);
-        }
-    },
+	async deleteAvatar(id) {
+		try {
+			const deleted = await api.delete(`/avatars/${id}`);
+			toast.success("Аватарка успешно удалена");
+			return deleted;
+		} catch (error) {
+			console.log(error);
+			toast.error("Ошибка при удалении аватарки");
+		}
+	},
 
-    async updateAvatar (id, data) {
-        try {
-            const updateAvatar = await api.put(`/avatars/${id}`, data)
+	async updateAvatar(id, data) {
+		try {
+			const updateAvatar = await api.put(`/avatars/${id}`, data);
+			toast.success("Аватарка успешно обновлена");
+			return updateAvatar.data;
+		} catch (error) {
+			console.log(error);
+			toast.error("Ошибка при обновлении аватарки");
+		}
+	},
 
-            return updateAvatar.data;
-        }
-        catch (error) {
-            console.log(error);
-        }
-    },
-
-    async createAvatar (data) {
-        try {
-            const newAvatar = await api.post('/avatars/', data)
-
-            return newAvatar;
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-}
+	async createAvatar(data) {
+		try {
+			const newAvatar = await api.post("/avatars/", data);
+			toast.success("Аватарка создана успешно");
+			return newAvatar;
+		} catch (error) {
+			console.log(error);
+			toast.error("Ошибка при создании аватарки");
+		}
+	},
+};
